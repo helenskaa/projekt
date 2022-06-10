9d1121ce50c39f417f5814d7895652b0206609f1
helenskaa <100362793+helenskaa@users.noreply.github.com>
2022-06-10 17:48:49 +0200

    unnötige html-datein gelöscht, neue dateistruktur gemacht und json erstellt

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask import render_template
-from flask import url_for
+from flask import url_for, request
+import json
+
 
 app = Flask("Quiz")
-
 
 @app.route("/")
 def home():
@@ -11,24 +12,32 @@ def home():
     return render_template("index.html", link=start_link)
 
 
-@app.route("/start")
+@app.route("/start", methods=["GET", "POST"])
 def start():
-    geografie_link = url_for("geografie")
-    kunstkultur_link = url_for("kunstkultur")
-    bücherfilmeserien_link = url_for("bücherfilmeserien")
-    return render_template("start.html", link=geografie_link, links=kunstkultur_link, linkss=bücherfilmeserien_link)
 
-@app.route("/goegrafie")
-def geografie():
-    return render_template("geografie.html")
+    return render_template("start.html")
 
-@app.route("/kunstkultur")
-def kunstkultur():
-    return render_template("kunstkultur.html")
+@app.route("/quiz", methods=["GET", "POST"])
+def quiz():
 
-@app.route("/bücherfilmeserien")
-def bücherfilmeserien():
-    return render_template("bücherfilmeserien.html")
+    try:
+        q = open("datenbank.json")
+        quizdatenbank = json.load(q)
+    except FileNotFoundError:
+        quizdatenbank = []
+
+    for element in quizdatenbank:
+        print(element)
+
+    with open("datenbank.json", "w") as d:
+        json.dump(quizdatenbank, d, indent=4, separators=(",", ":"))
+
+    return render_template("quiz.html")
+
+@app.route("/auswertung", methods=["GET", "POST"])
+def auswertung():
+
+    return render_template("auswertung.html")
 
 
 if __name__ == "__main__":
